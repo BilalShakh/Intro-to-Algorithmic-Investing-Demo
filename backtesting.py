37405a4ef62fca8dab8779e269deae0be323ea6a
BilalShakh <bilalzaki92@gmail.com>
2022-01-17 02:27:03 -0500

    Restructured code

--- backtesting.py
+++ backtesting.py
@@ -1,4 +1,5 @@
 import datetime as dt
+from sqlite3 import Date
 import yfinance as yf
 from pandas_datareader import data as pdr
 import plotly.express as px
@@ -6,120 +7,135 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
 
-#Initializing data needed for strategy.
-yf.pdr_override()
-stock="TSLA"
-start=dt.datetime(2021,1,1)
-end=dt.datetime.now()
+class BackTest:
+    def __init__(self, stock, startDate, endDate):
+        yf.pdr_override()
+        self.stock = stock
+        self.startDate = startDate
+        self.endDate = endDate
 
-df=pdr.get_data_yahoo(stock,start,end)
+        self.df = pdr.get_data_yahoo(stock, self.startDate, self.endDate)
 
-df['EMA_12']=df.iloc[:,4].ewm(span=12, adjust=False).mean()
-df['EMA_26']=df.iloc[:,4].ewm(span=26, adjust=False).mean()
-df['MACD']=df.iloc[:,6]-df.iloc[:,7]
-df['SIGNAL']=df.iloc[:,8].ewm(span=9, adjust=False).mean()
-df['DIFFSM']=df.iloc[:,8]-df.iloc[:,9]
+        self.df['EMA_12'] = self.df.iloc[:,4].ewm(span=12, adjust=False).mean()
+        self.df['EMA_26'] = self.df.iloc[:,4].ewm(span=26, adjust=False).mean()
+        self.df['MACD'] = self.df.iloc[:,6] - self.df.iloc[:,7]
+        self.df['SIGNAL'] = self.df.iloc[:,8].ewm(span=9, adjust=False).mean()
+        self.df['DIFFSM'] = self.df.iloc[:,8] - self.df.iloc[:,9]
 
-#Setting up variables to tract the progress of trading strategy and anything else needed for the strategy.
-holding=False
-loc=0
-res=[]
-buyingPrice = -1
-lenDF = df["MACD"].count()
-
-
-BuyDate=[]
-BuyPrice=[]
-SellDate=[]
-SellPrice=[]
-
-#print(df.tail())
-#Running the strategy through the dataset.
-for i in df.index:
-    close = df["Close"][i]
-    currMacd = df["MACD"][i]
-    currSignal = df["SIGNAL"][i]
+        self.BuyDate = []
+        self.BuyPrice = []
+        self.SellDate = []
+        self.SellPrice = []
+        self.tradeRes = []
     
-    if (currMacd > currSignal):
-        if not holding:
-            holding = True
-            buyingPrice = close
-            BuyDate.append(i)
-            BuyPrice.append(buyingPrice)
-    elif (currMacd < currSignal):
-        if holding:
-            holding = False
-            gain = 100 * (close - buyingPrice)/buyingPrice
-            SellDate.append(i)
-            SellPrice.append(close)
-            res.append(round(gain,3))
+    def buyStock(self, date, price):
+        self.BuyDate.append(date)
+        self.BuyPrice.append(price)
     
-    if (loc == lenDF - 1 and holding):
-        holding = False
-        gain = 100 * (close - buyingPrice)/buyingPrice
-        SellDate.append(i)
-        SellPrice.append(close)
-        res.append(round(gain,3))
+    def sellStock(self, date, price, buyCost):
+        gain = 100 * (price - buyCost)/buyCost
+        self.SellDate.append(date)
+        self.SellPrice.append(price)
+        self.tradeRes.append(round(gain, 3))
+
+    def runAlgorithm(self):
+        holding=False
+        loc=0
+        buyingCost=-1
+        lendf = self.df["MACD"].count()
+        
+        for i in self.df.index:
+            close = self.df["Close"][i]
+            currMacd = self.df["MACD"][i]
+            currSignal = self.df["SIGNAL"][i]
+
+            if (currMacd > currSignal):
+                if not holding:
+                    holding = True
+                    buyingCost = close
+                    self.buyStock(i, close)
+            elif (currMacd < currSignal):
+                if holding:
+                    holding = False
+                    self.sellStock(i, close, buyingCost)
+
+            if (loc == lendf - 1 and holding):
+                holding = False
+                self.sellStock(i, close, buyingCost)
+
+            loc += 1
+
+        self.df.insert(11, "Date", self.df.index, False)
+
+    def getResults(self):
+        tradesWon = 0
+        numTrades = len(self.tradeRes)
+
+        for i in range(numTrades):
+            if (self.tradeRes[i] > 0):
+                tradesWon += 1        
+        winRate = (tradesWon / numTrades) * 100
+
+        maxReturn = self.tradeRes[0]
+        maxDate = str(self.SellDate[0])
+        avgReturn = sum(self.tradeRes)/numTrades
+        
+        for i in range(1, numTrades):
+            if (self.tradeRes[i] > maxReturn):
+                maxDate = str(self.SellDate[i])
+                maxReturn = self.tradeRes[i]
+
+        print("Results for trading "+ self.stock + " from " + str(self.startDate) + " to " + str(self.endDate) + ":")
+        print("Max Return on " + maxDate + " earning a gain of " + str(round(maxReturn,2)) + "%.")
+        print("This strategy has a winrate of "+ str(winRate) + "% by placing " + str(numTrades) + " trades and earned an average return of " + str(round(avgReturn,2)) + "%.")
     
-    loc += 1
-    
-df.insert(11,"Date",df.index, False)
-
-#Calculating %profit and other useful stai
-maxReturn = res[0]
-maxDate = str(SellDate[0])
-avgReturn = sum(res)/len(res)
-for i in range(1,len(res)):
-    if res[i] > maxReturn:
-        maxReturn = res[i]
-        maxDate = str(SellDate[i])
-
-
-#print(df.head())
-print("Max Return on " + maxDate + " earning a gain of " + str(round(maxReturn,2)) + "%.")
-print("This strategy earned an average return of " + str(round(avgReturn,2)) + "% by placing " + str(len(res)) + " trades from " + str(start) + " to " + str(end) +".") 
-
-candlestickFig = go.Figure(data=[go.Candlestick(x=df['Date'],
-                open=df['Open'],
-                high=df['High'],
-                low=df['Low'],
-                close=df['Close'])])
-EMA_12Fig = px.line(df, x='Date', y="EMA_12")
-EMA_12Fig.update_traces(line=dict(color = 'rgba(238,130,238,0.7)'))
-EMA_26Fig = px.line(df, x='Date', y="EMA_26")
-BuyFig = go.Figure(data=go.Scatter(x=BuyDate, y=BuyPrice, mode='markers'))
-BuyFig.update_traces(marker=dict(color = 'rgba(60, 179, 113,1)', size=9, line=dict(width=1,
-                                        color='DarkSlateGrey')))
-SellFig = go.Figure(data=go.Scatter(x=SellDate, y=SellPrice, mode='markers'))
-SellFig.update_traces(marker=dict(color = 'rgba(255, 0, 0,1)', size=9, line=dict(width=1,
-                                        color='DarkSlateGrey')))
-topFig = go.Figure(data=EMA_12Fig.data + EMA_26Fig.data + candlestickFig.data + BuyFig.data + SellFig.data)
-topFig.update_layout(xaxis_rangeslider_visible=False)
+    def createTradingView(self):
+        candlestickFig = go.Figure(data=[go.Candlestick(x=self.df['Date'],
+                open=self.df['Open'],
+                high=self.df['High'],
+                low=self.df['Low'],
+                close=self.df['Close'])])
+        EMA_12Fig = px.line(self.df, x='Date', y="EMA_12")
+        EMA_12Fig.update_traces(line=dict(color = 'rgba(238,130,238,0.7)'))
+        EMA_26Fig = px.line(self.df, x='Date', y="EMA_26")
+        BuyFig = go.Figure(data=go.Scatter(x=self.BuyDate, y=self.BuyPrice, mode='markers'))
+        BuyFig.update_traces(marker=dict(color = 'rgba(60, 179, 113,1)', size=9, line=dict(width=1,
+                                                color='DarkSlateGrey')))
+        SellFig = go.Figure(data=go.Scatter(x=self.SellDate, y=self.SellPrice, mode='markers'))
+        SellFig.update_traces(marker=dict(color = 'rgba(255, 0, 0,1)', size=9, line=dict(width=1,
+                                                color='DarkSlateGrey')))
+        topFig = go.Figure(data=EMA_12Fig.data + EMA_26Fig.data + candlestickFig.data + BuyFig.data + SellFig.data)
+        topFig.update_layout(xaxis_rangeslider_visible=False)
 
 
-macDFig=px.line(df, x='Date', y="MACD")
-signalFig=px.line(df, x='Date', y="SIGNAL")
-signalFig.update_traces(line=dict(color = 'rgba(255,165,0,0.7)'))
-df["Color"]=np.where(df["DIFFSM"]<0, 'rgb(255, 0, 0)', 'rgb(60, 179, 113)')
-DiffFig = go.Figure()
-DiffFig.add_trace(
-    go.Bar(x=df['Date'],
-           y=df['DIFFSM'],
-           marker_color=df['Color']))
-DiffFig.update_layout(barmode='stack')
-botFig=go.Figure(data=macDFig.data + signalFig.data + DiffFig.data)
+        macdfig=px.line(self.df, x='Date', y="MACD")
+        signalFig=px.line(self.df, x='Date', y="SIGNAL")
+        signalFig.update_traces(line=dict(color = 'rgba(255,165,0,0.7)'))
+        self.df["Color"]=np.where(self.df["DIFFSM"]<0, 'rgb(255, 0, 0)', 'rgb(60, 179, 113)')
+        DiffFig = go.Figure()
+        DiffFig.add_trace(
+            go.Bar(x=self.df['Date'],
+                y=self.df['DIFFSM'],
+                marker_color=self.df['Color']))
+        DiffFig.update_layout(barmode='stack')
+        botFig=go.Figure(data=macdfig.data + signalFig.data + DiffFig.data)
 
-combinedFig = make_subplots(rows=2, cols=1,
-                    shared_xaxes=True,
-                    vertical_spacing=0.02,
-                    row_heights=[0.7,0.3],
-                    shared_yaxes=False)
+        combinedfig = make_subplots(rows=2, cols=1,
+                            shared_xaxes=True,
+                            vertical_spacing=0.02,
+                            row_heights=[0.7,0.3],
+                            shared_yaxes=False)
 
-#EMA_12Fig = px.l
-for i in range(len(topFig.data)):
-    combinedFig.add_trace(topFig.data[i], row=1, col=1)
-for i in range(len(botFig.data)):
-    combinedFig.add_trace(botFig.data[i], row=2, col=1)
-combinedFig.update_layout(xaxis_rangeslider_visible=False)
-combinedFig.update_xaxes(range=[start, end], row=2, col=1)
-combinedFig.show()
+        #EMA_12Fig = px.l
+        for i in range(len(topFig.data)):
+            combinedfig.add_trace(topFig.data[i], row=1, col=1)
+        for i in range(len(botFig.data)):
+            combinedfig.add_trace(botFig.data[i], row=2, col=1)
+        combinedfig.update_layout(xaxis_rangeslider_visible=False)
+        combinedfig.update_xaxes(range=[self.startDate, self.endDate], row=2, col=1)
+        combinedfig.show()
+
+testObject = BackTest("TSLA", dt.datetime(2011,1,1), dt.datetime.now())
+testObject.runAlgorithm()
+testObject.getResults()
+testObject.createTradingView()
